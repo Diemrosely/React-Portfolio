31d8ba2682800c646b09d617442d186e095a7b19
Diem Ly <Diemrosely@gmail.com>
2020-08-28 20:07:35 -0600

    build

---
+++ build/precache-manifest.bc88a004a73ea45eecd5bb1ff484be53.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fbb8a0b98c23c79642fd581405da68b",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "e5995791bf931ef65d3c",
+    "url": "/React-Portfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "f35db37585c7b6f9049d",
+    "url": "/React-Portfolio/static/css/main.fb223e49.chunk.css"
+  },
+  {
+    "revision": "e5995791bf931ef65d3c",
+    "url": "/React-Portfolio/static/js/2.fdfddf2d.chunk.js"
+  },
+  {
+    "revision": "2f71fbc01fec0c62d6e32185bc021541",
+    "url": "/React-Portfolio/static/js/2.fdfddf2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f35db37585c7b6f9049d",
+    "url": "/React-Portfolio/static/js/main.22472715.chunk.js"
+  },
+  {
+    "revision": "b63d20c95b4097aaed6d",
+    "url": "/React-Portfolio/static/js/runtime-main.67dc4218.js"
+  },
+  {
+    "revision": "08d4ea99e3e4a9ed3972668d616397d0",
+    "url": "/React-Portfolio/static/media/DiemLyResume.08d4ea99.pdf"
+  },
+  {
+    "revision": "f090c10848f0612603d4ba9d96afa2f7",
+    "url": "/React-Portfolio/static/media/profile.f090c108.jpeg"
+  }
+]);
